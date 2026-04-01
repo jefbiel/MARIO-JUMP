9854611ed23e2019c4ff07e1219550dfe9218569
jefbiel <143344048+jefbiel@users.noreply.github.com>
2026-04-01 00:47:27 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const mario = document.querySelector(".mario"); 
+
+const jump = () => {
+    mario.classList.add("jump"); 
+
+    setTimeout(() => {
+
+        mario.classList.remove("jump");
+
+    }, 0.5);  
+
+
+}
+document.addEventListener("keydown", jump); 
